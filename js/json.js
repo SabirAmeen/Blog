040d8b6290fec4c2884eb43f8fb06c2ae19f5cf8
Sabir <sabir@qburst.com>
2016-08-04 17:08:14 +0530

    Final Version

---
+++ js/json.js
@@ -0,0 +1,19 @@
+ var json={
+  "link": [
+    {
+      "url": "images/banner.jpg"
+    },
+    {
+      "url": "images/1136.jpg"
+    },
+    {
+      "url": "images/images.jpg"
+    },
+    {
+      "url": "images/sb.jpg"
+    },
+    {
+      "url": "images/peace.jpg"
+    }
+  ]
+};
--- js/works.js
+++ js/works.js
@@ -1,5 +1,18 @@
-var comm=document.getElementById("comm");
-function showcomm()
-{
-	comm.style.display="block";
-}
+function myFunction() {
+    var img1=document.getElementById("i1");
+var img2=document.getElementById("i2");
+var img3=document.getElementById("i3");
+var img4=document.getElementById("i4");
+var con=document.getElementById("js");
+var url = "https://api.myjson.com/bins/2xzgv";
+var Httpreq = new XMLHttpRequest(); // a new request
+Httpreq.open("GET",url,false);
+Httpreq.send();
+var myArr = JSON.parse(Httpreq.responseText);
+   var len=myArr.length;
+    img1.src=myArr.link[0].url;
+        img2.src=myArr.link[1].url;
+        img3.src=myArr.link[2].url;
+            img4.src=myArr.link[3].url;
+   			
+}	
